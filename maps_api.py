ead4b20e4ea43e6a44a2e15731085cd840b68c0d
Ilya <smirnovilya7031@gmail.com>
2023-02-10 17:25:29 +0500

    map and map scale

--- maps_api.py
+++ maps_api.py
@@ -1,5 +1,21 @@
 import pygame
 import sys
+import requests
+import os
+from PIL import Image
+
+map_request = "http://static-maps.yandex.ru/1.x/?ll=133.316901,-27.543700&spn=150,0.002&l=sat"
+response = requests.get(map_request)
+
+if not response:
+    print("Ошибка выполнения запроса:")
+    print(map_request)
+    print("Http статус:", response.status_code, "(", response.reason, ")")
+    sys.exit(1)
+
+def pil_image_to_surface(pil_image):
+    mode, size, data = pil_image.mode, pil_image.size, pil_image.tobytes()
+    return pygame.image.fromstring(data, size, mode).convert_alpha()
 
 
 class Button():
@@ -21,14 +37,10 @@ class Button():
         self.buttonSurface = self.buttonSurface.convert_alpha()
         if not self.buttonRect.collidepoint(mouse_pos):
             self.buttonSurface.fill((0, 0, 0, 0))
-            font = pygame.font.SysFont("arial", 45)
+            font = pygame.font.SysFont("arial", 25)
             self.buttonSurf = font.render(self.buttonText, True, (255, 255, 255))
         if self.buttonRect.collidepoint(mouse_pos):
-            self.buttonSurface.fill((0, 39, 62, 0))
-            shape_surf = pygame.Surface(pygame.Rect(0, self.y, pygame.display.Info().current_w, self.height).size,
-                                        pygame.SRCALPHA)
-            pygame.draw.rect(shape_surf, (0, 39, 62, 230), shape_surf.get_rect())
-            screen.blit(shape_surf, (0, self.y, pygame.display.Info().current_w, self.height))
+            self.buttonSurface.fill((0, 39, 62, 150))
             if pygame.mouse.get_pressed(num_buttons=3)[0]:
                 if not self.alreadyPressed:
                     self.onclickFunction()
@@ -50,14 +62,20 @@ def yet():
 
 if __name__ == "__main__":
     pygame.init()
-    width, height = 800, 800
-    screen = pygame.display.set_mode((width, height))
+    width, height = 1200, 800
+    screen = pygame.display.set_mode((width, height), pygame.RESIZABLE)
+    map_file = "map.png"
+    with open(map_file, "wb") as file:
+        file.write(response.content)
+    pil_image = Image.open(map_file)
+    pil_image = pil_image.resize((width, height))
+    pygame_image = pil_image_to_surface(pil_image.convert('RGBA'))
     pygame.display.set_caption("Я карта")
     objects = []
     fps = 60
     clock = pygame.time.Clock()
-    Button(width / 2, height / 2, 'Nothing', nothing)
-    Button(width / 2, height / 2, 'Yet', yet)
+    Button(width *  0.8, height * 0.008, 'Nothing', nothing)
+    Button(width *  0.9, height * 0.008, 'Yet', yet)
     running = True
     while running:
         screen.fill((0, 0, 0))
@@ -67,8 +85,14 @@ if __name__ == "__main__":
                 sys.exit()
             if event.type == pygame.KEYDOWN:
                 pass
+        screen.blit(pygame_image, (0, 0))
+        width, height = pygame.display.get_surface().get_size()
+        pil_image = Image.open(map_file)
+        pil_image = pil_image.resize((width, height))
+        pygame_image = pil_image_to_surface(pil_image.convert('RGBA'))
         for object in objects:
             object.process()
         pygame.display.flip()
         clock.tick(fps)
+    os.remove(map_file)
 
